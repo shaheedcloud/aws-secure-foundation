8482af9e9c8f07d303a26be2b1e9d5e89e2b5aa5
Shaheed Hayatuddin <116326189+shaheedcloud@users.noreply.github.com>
2025-08-18 20:33:37 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name prefix for all resources"
+  type        = string
+  default     = "aws-secure-foundation"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs (one per AZ)"
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs (one per AZ)"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+# Inputs we’ll use everywhere. Two AZs = 2 public + 2 private subnets.
